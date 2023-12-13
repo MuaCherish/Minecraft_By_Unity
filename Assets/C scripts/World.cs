d06d1f2b577184135de1c9e405d6e945472299b7
CryMellory <317426168@qq.com>
2023-12-13 21:42:21 +0800

    贴图的添加，和基本的分层结构

--- Assets/C scripts/World.cs
+++ Assets/C scripts/World.cs
@@ -5,53 +5,54 @@ using UnityEngine;
 public class World : MonoBehaviour
 {
 
-    //public Material material;
-    //public BlockType[] blocktypes;
+    public Material material;
+    public BlockType[] blocktypes;
 
 }
 
-//[System.Serializable]
-//public class BlockType
-//{
+//结构体BlockType
+//存储方块种类+面对应的UV
+[System.Serializable]
+public class BlockType
+{
 
-//    public string blockName;
-//    public bool isSolid;
+    public string blockName;
+    public bool isSolid;
 
-//    [Header("Texture Values")]
-//    public int backFaceTexture;
-//    public int frontFaceTexture;
-//    public int topFaceTexture;
-//    public int bottomFaceTexture;
-//    public int leftFaceTexture;
-//    public int rightFaceTexture;
+    [Header("Texture Values")]
+    public int backFaceTexture;
+    public int frontFaceTexture;
+    public int topFaceTexture;
+    public int bottomFaceTexture;
+    public int leftFaceTexture;
+    public int rightFaceTexture;
 
-//    // Back, Front, Top, Bottom, Left, Right
+    //贴图中的面的坐标
+    public int GetTextureID(int faceIndex)
+    {
 
-//    public int GetTextureID(int faceIndex)
-//    {
+        switch (faceIndex)
+        {
 
-//        switch (faceIndex)
-//        {
-
-//            case 0:
-//                return backFaceTexture;
-//            case 1:
-//                return frontFaceTexture;
-//            case 2:
-//                return topFaceTexture;
-//            case 3:
-//                return bottomFaceTexture;
-//            case 4:
-//                return leftFaceTexture;
-//            case 5:
-//                return rightFaceTexture;
-//            default:
-//                Debug.Log("Error in GetTextureID; invalid face index");
-//                return 0;
+            case 0:
+                return backFaceTexture;
+            case 1:
+                return frontFaceTexture;
+            case 2:
+                return topFaceTexture;
+            case 3:
+                return bottomFaceTexture;
+            case 4:
+                return leftFaceTexture;
+            case 5:
+                return rightFaceTexture;
+            default:
+                Debug.Log("Error in GetTextureID; invalid face index");
+                return 0;
 
 
-//        }
+        }
 
-//    }
+    }
 
-//}
+}
