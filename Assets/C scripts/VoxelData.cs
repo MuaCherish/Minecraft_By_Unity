c03f4ec2a12eed4585b7b1d739ce712b20db18a7
CryMellory <317426168@qq.com>
2023-12-13 16:43:18 +0800

    unity更新导致文件丢失，测试一下

---
+++ Assets/C scripts/VoxelData.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class VoxelData
+{
+
+    //chunk大小
+    public static readonly int ChunkWidth = 5;
+    public static readonly int ChunkHeight = 15;
+
+
+
+    //顶点数组
+    public static readonly Vector3[] voxelVerts = new Vector3[8] {
+
+        new Vector3(0.0f, 0.0f, 0.0f),
+        new Vector3(1.0f, 0.0f, 0.0f),
+        new Vector3(1.0f, 1.0f, 0.0f),
+        new Vector3(0.0f, 1.0f, 0.0f),
+        new Vector3(0.0f, 0.0f, 1.0f),
+        new Vector3(1.0f, 0.0f, 1.0f),
+        new Vector3(1.0f, 1.0f, 1.0f),
+        new Vector3(0.0f, 1.0f, 1.0f),
+
+    };
+
+
+    //指定方向
+    public static readonly Vector3[] faceChecks = new Vector3[6]
+    {
+
+        new Vector3(0.0f, 0.0f, -1.0f),
+        new Vector3(0.0f, 0.0f, 1.0f),
+        new Vector3(0.0f, 1.0f, 0.0f),
+        new Vector3(0.0f, -1.0f, 0.0f),
+        new Vector3(-1.0f, 0.0f, 0.0f),
+        new Vector3(1.0f, 0.0f, 0.0f),
+
+    };
+
+
+
+
+    //绘制序列
+    public static readonly int[,] voxelTris = new int[6, 4] {
+
+		{0, 3, 1, 2}, // Back Face
+		{5, 6, 4, 7}, // Front Face
+		{3, 7, 2, 6}, // Top Face
+		{1, 5, 0, 4}, // Bottom Face
+		{4, 7, 0, 3}, // Left Face
+		{1, 2, 5, 6}  // Right Face
+
+	};
+
+    //UV数组
+    public static readonly Vector2[] voxelUvs = new Vector2[4] {
+
+        new Vector2 (0.0f, 0.0f),
+        new Vector2 (0.0f, 1.0f),
+        new Vector2 (1.0f, 0.0f),
+        new Vector2 (1.0f, 1.0f)
+
+    };
+}
